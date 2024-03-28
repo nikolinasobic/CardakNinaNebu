d646d6d2ea6279ade21cdbb532a648131488f6e4
nikolinasobic <nikolinasobic@gmail.com>
2024-03-28 17:37:32 +0100

    skybox

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,8 @@ void processInput(GLFWwindow *window);
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
 
+unsigned int loadCubemap(vector<std::string> faces);
+
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -171,6 +173,8 @@ int main() {
     // build and compile shaders
     // -------------------------
     Shader ourShader("resources/shaders/model_shader.vs", "resources/shaders/model_shader.fs");
+    Shader skyboxShader("resources/shaders/skyboxShader.vs", "resources/shaders/skyboxShader.fs");
+
 
     // load models
     // -----------
@@ -196,6 +200,83 @@ int main() {
     Model oblak("resources/objects/oblak/source/Cloud.obj");
     oblak.SetShaderTextureNamePrefix("material.");
     stbi_set_flip_vertically_on_load(true);
+
+
+
+    // parametri za skybox
+    float skyboxVertices[] = {
+            // positions
+            -100.0f,  100.0f, -100.0f,
+            -100.0f, -100.0f, -100.0f,
+            100.0f, -100.0f, -100.0f,
+            100.0f, -100.0f, -100.0f,
+            100.0f,  100.0f, -100.0f,
+            -100.0f,  100.0f, -100.0f,
+
+            -100.0f, -100.0f,  100.0f,
+            -100.0f, -100.0f, -100.0f,
+            -100.0f,  100.0f, -100.0f,
+            -100.0f,  100.0f, -100.0f,
+            -100.0f,  100.0f,  100.0f,
+            -100.0f, -100.0f,  100.0f,
+
+            100.0f, -100.0f, -100.0f,
+            100.0f, -100.0f,  100.0f,
+            100.0f,  100.0f,  100.0f,
+            100.0f,  100.0f,  100.0f,
+            100.0f,  100.0f, -100.0f,
+            100.0f, -100.0f, -100.0f,
+
+            -100.0f, -100.0f,  100.0f,
+            -100.0f,  100.0f,  100.0f,
+            100.0f,  100.0f,  100.0f,
+            100.0f,  100.0f,  100.0f,
+            100.0f, -100.0f,  100.0f,
+            -100.0f, -100.0f,  100.0f,
+
+            -100.0f,  100.0f, -100.0f,
+            100.0f,  100.0f, -100.0f,
+            100.0f,  100.0f,  100.0f,
+            100.0f,  100.0f,  100.0f,
+            -100.0f,  100.0f,  100.0f,
+            -100.0f,  100.0f, -100.0f,
+
+            -100.0f, -100.0f, -100.0f,
+            -100.0f, -100.0f,  100.0f,
+            100.0f, -100.0f, -100.0f,
+            100.0f, -100.0f, -100.0f,
+            -100.0f, -100.0f,  100.0f,
+            100.0f, -100.0f,  100.0f
+    };
+
+    unsigned int skyboxVAO, skyboxVBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+
+    glBindVertexArray(skyboxVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+
+    vector<std::string> faces_sky {
+            FileSystem::getPath("resources/textures/skybox/px.png"),
+            FileSystem::getPath("resources/textures/skybox/nx.png"),
+            FileSystem::getPath("resources/textures/skybox/py.png"),
+            FileSystem::getPath("resources/textures/skybox/ny.png"),
+            FileSystem::getPath("resources/textures/skybox/pz.png"),
+            FileSystem::getPath("resources/textures/skybox/nz.png")
+    };
+
+    stbi_set_flip_vertically_on_load(false);
+    unsigned int cubemapTextureSky = loadCubemap(faces_sky);
+    stbi_set_flip_vertically_on_load(true);
+    skyboxShader.use();
+    skyboxShader.setInt("skybox", 0);
 
 
 
@@ -305,6 +386,28 @@ int main() {
         model = glm::scale(model, glm::vec3(0.5f));
         ourShader.setMat4("model", model);
         oblak.Draw(ourShader);
+
+
+
+        //sejder se renderuje poslednji
+
+        glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
+        skyboxShader.use();
+        view = glm::mat4(glm::mat3(programState->camera.GetViewMatrix())); // remove translation from the view matrix
+        skyboxShader.setMat4("view", view);
+        skyboxShader.setMat4("projection", projection);
+        // skybox cube
+        glBindVertexArray(skyboxVAO);
+        glActiveTexture(GL_TEXTURE0);
+
+
+
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTextureSky);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+        glDepthFunc(GL_LESS); // set depth function back to default
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
 
         if (programState->ImGuiEnabled)
@@ -438,4 +541,35 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
             glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
         }
     }
+}
+
+
+unsigned int loadCubemap(vector<std::string> faces)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    int width, height, nrChannels;
+    for (unsigned int i = 0; i < faces.size(); i++)
+    {
+        unsigned char *data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return textureID;
 }
